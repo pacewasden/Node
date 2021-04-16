775d41fc72088196223f20a24744a6c7d137b7df
Pace Wasden <pacewasden@gmail.com>
2021-04-16 10:21:09 -0600

    Working with Yargs

--- Node-App/app.js
+++ Node-App/app.js
@@ -1,9 +1,34 @@
 // const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-const msg = getNotes()
-
-console.log(chalk.redBright.inverse('this worked'))
-// console.log(validator.isURL('pacewasdenil.com'))
-console.log(process.argv)
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    handler: function(){
+        console.log('Adding a new note.')
+    }
+})
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: function(){
+        console.log('Removed note.')
+    }
+})
+yargs.command({
+    command: 'list',
+    describe: 'List of notes',
+    handler: function(){
+        console.log('Listing all notes.')
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'Read notes',
+    handler: function(){
+        console.log('Reading the notes')
+    }
+})
+console.log(yargs.argv)
